2ed8c12d3f8f26783d15600bd4ca00a2c45cbad3
Rashid Doniyorov <rashiddoniyorov05@gmail.com>
2026-02-13 16:58:00 +0500

    Same Tree masalasi rekursiya orqali ishlandi

---
+++ easy/0100-same-tree.py
@@ -0,0 +1,20 @@
+"""
+Problem: 100. Same Tree
+URL: https://leetcode.com/problems/same-tree/
+Description: Given the roots of two binary trees p and q, write a function to check if they are the same or not. Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+Difficulty: Easy
+"""
+
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        
+class Solution(object):
+    def isSameTree(self, p, q):
+        if not p and not q:
+            return True
+        if not p or not q or p.val != q.val:
+            return False
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
